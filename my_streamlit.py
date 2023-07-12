321dbd1965f8afce27519e1cade8d808f1362ff2
Borja-F <134072233+Borja-F@users.noreply.github.com>
2023-07-12 18:30:46 +0200

    Update my_streamlit.py

--- my_streamlit.py
+++ my_streamlit.py
@@ -1,29 +1,14 @@
 import streamlit as st
-from PIL import Image
 import pandas as pd
-import numpy as np
 import folium
-import streamlit_folium
-
 from streamlit_folium import folium_static
-
 
 st.set_page_config(
     page_title="Mapa Valencia",
     page_icon="map",
     layout="wide",
-    initial_sidebar_state="auto",
-    
+    initial_sidebar_state="auto"
 )
-
-
-
-df = pd.read_csv("colegioslimpios.csv", sep=",")
-
-
-
-
-
 
 # Coordenadas de Valencia
 latitud = 39.4699
@@ -31,13 +16,9 @@ longitud = -0.3763
 
 # Crear un objeto de mapa centrado en Valencia
 mapa_valencia = folium.Map(location=[latitud, longitud], zoom_start=12)
-st.markdown(mapa_valencia._repr_html_(), unsafe_allow_html=True)
 
-# Crear una capa para los marcadores
-capa_marcadores = folium.FeatureGroup(name='Marcadores')
-
-# Añadir un marcador en Valencia a la capa de marcadores
-folium.Marker(location=[latitud, longitud], popup='Valencia').add_to(capa_marcadores)
+# Añadir un marcador en Valencia
+folium.Marker(location=[latitud, longitud], popup='Valencia').add_to(mapa_valencia)
 
 # Colocar Marcadores
 marcador_1 = folium.Marker(location=[39.470000, -0.370000], 
@@ -58,8 +39,8 @@ marcador_5 = folium.Marker(location=[39.544678, -0.331139],
 marcador_6 = folium.Marker(location=[39.470000, -0.395306], 
             popup='Valencia-La Roqueta', 
             icon=folium.Icon(color='green', icon='6', prefix='fa'))
-marcador_7 = folium.Marker(location=[39.475736	, -0.427417], 
-            popup='Mislata ', 
+marcador_7 = folium.Marker(location=[39.475736, -0.427417], 
+            popup='Mislata', 
             icon=folium.Icon(color='green', icon='7', prefix='fa'))
 marcador_8 = folium.Marker(location=[39.481541, -0.460455], 
             popup='Quart de Poblet', 
@@ -68,28 +49,18 @@ marcador_9 = folium.Marker(location=[39.424049, -0.496320],
             popup='Torrent', 
             icon=folium.Icon(color='green', icon='9', prefix='fa'))
 
+# Añadir los marcadores al mapa
+mapa_valencia.add_child(marcador_1)
+mapa_valencia.add_child(marcador_2)
+mapa_valencia.add_child(marcador_3)
+mapa_valencia.add_child(marcador_4)
+mapa_valencia.add_child(marcador_5)
+mapa_valencia.add_child(marcador_6)
+mapa_valencia.add_child(marcador_7)
+mapa_valencia.add_child(marcador_8)
+mapa_valencia.add_child(marcador_9)
 
-
-# Añadir los marcadores a la capa de marcadores
-capa_marcadores.add_child(marcador_1)
-capa_marcadores.add_child(marcador_2)
-capa_marcadores.add_child(marcador_3)
-capa_marcadores.add_child(marcador_4)
-capa_marcadores.add_child(marcador_5)
-capa_marcadores.add_child(marcador_6)
-capa_marcadores.add_child(marcador_7)
-capa_marcadores.add_child(marcador_8)
-capa_marcadores.add_child(marcador_9)
-
-# Añadir la capa de marcadores al mapa
-mapa_valencia.add_child(capa_marcadores)
-
-# Añadir control de capas al mapa
-folium.LayerControl().add_to(mapa_valencia)
-st.markdown(mapa_valencia._repr_html_(), unsafe_allow_html=True)
-
-# Mostrar el mapa
-
+# Mostrar el mapa en Streamlit
 st.folium_chart(mapa_valencia)
 
 
