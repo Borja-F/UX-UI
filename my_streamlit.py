fca7ac6177378eae364ead21cd399c88b751c12b
Borja-F <134072233+Borja-F@users.noreply.github.com>
2023-07-12 18:07:01 +0200

    Add files via upload

--- my_streamlit.py
+++ my_streamlit.py
@@ -44,19 +44,24 @@ elif option == "Charts":
 
     
 
-    # Coordenadas de Valencia
-    latitud = 39.4699
-    longitud = -0.3763
+    mapa_valencia = folium.Map(location=[39.47825415129413, -0.36984913737192593],  zoom_start=16)
 
-    # Crear un objeto de mapa centrado en Valencia
-    mapa_valencia = folium.Map(location=[latitud, longitud], zoom_start=12)
-    st.markdown(mapa_valencia._repr_html_(), unsafe_allow_html=True)
+    fg = folium.FeatureGroup(name='Colegios')
+    mapa_valencia.add_child(fg)
 
-    # Crear una capa para los marcadores
-    capa_marcadores = folium.FeatureGroup(name='Marcadores')
+    zona_segura = folium.FeatureGroup(name='Zonas Seguras')
 
-    # Añadir un marcador en Valencia a la capa de marcadores
-    folium.Marker(location=[latitud, longitud], popup='Valencia').add_to(capa_marcadores)
+    paradas_metro = folium.FeatureGroup(name='Paradas Metro')
+    mapa_valencia.add_child(paradas_metro)
+    # Ruta al archivo GeoJSON
+    archivo_geojson = 'fgv-bocas.geojson'
+
+    # Crear una capa a partir del archivo GeoJSON
+    capa_geojson = folium.GeoJson(archivo_geojson)
+
+    # Añadir la capa al mapa
+    capa_geojson.add_to(paradas_metro)
+
 
     # Colocar Marcadores
     marcador_1 = folium.Marker(location=[39.470000, -0.370000], 
@@ -90,25 +95,39 @@ elif option == "Charts":
 
 
     # Añadir los marcadores a la capa de marcadores
-    capa_marcadores.add_child(marcador_1)
-    capa_marcadores.add_child(marcador_2)
-    capa_marcadores.add_child(marcador_3)
-    capa_marcadores.add_child(marcador_4)
-    capa_marcadores.add_child(marcador_5)
-    capa_marcadores.add_child(marcador_6)
-    capa_marcadores.add_child(marcador_7)
-    capa_marcadores.add_child(marcador_8)
-    capa_marcadores.add_child(marcador_9)
+    zona_segura.add_child(marcador_1)
+    zona_segura.add_child(marcador_2)
+    zona_segura.add_child(marcador_3)
+    zona_segura.add_child(marcador_4)
+    zona_segura.add_child(marcador_5)
+    zona_segura.add_child(marcador_6)
+    zona_segura.add_child(marcador_7)
+    zona_segura.add_child(marcador_8)
+    zona_segura.add_child(marcador_9)
 
     # Añadir la capa de marcadores al mapa
-    mapa_valencia.add_child(capa_marcadores)
+    mapa_valencia.add_child(zona_segura)
 
-    # Añadir control de capas al mapa
+    # Colocamos los colegios
+    for (index, row) in mask.iterrows():
+        folium.Marker(location = [row.loc['lat'], row.loc['long']],
+                    popup = row.loc['Denominacion_Generica_ES'], icon=folium.Icon(color='purple'),
+                    tooltip = row['Denominacion'], name = "Colegios").add_to(fg)
+
+    def style_function(style_function): #Definimos la función para cambiar el color a verde
+        return {
+            'shadowColor': '#00ff00'
+        }
+    style2 = {'fillColor': '#008000', 'color': '#008000'}
+
+    folium.GeoJson('zonas-verdes.geojson', name='Zonas verdes',style_function=lambda x:style2).add_to(mapa_valencia)
+
+
+    folium.GeoJson('be-bien-bic.geojson', name='Bienes culturales').add_to(mapa_valencia)
     folium.LayerControl().add_to(mapa_valencia)
-
-    folium_static(mapa_valencia)
-
-    # Mostrar el mapa
+        
+    # display map    
+    mapa_valencia
     
 
     
